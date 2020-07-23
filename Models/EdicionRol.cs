2222334c154a776ccb7fa93eb049b3446cf23ae1
jhonacosta99 <jhonacosta99@outlook.es>
2020-07-22 22:15:39 -0500

    primer commit

---
+++ Models/EdicionRol.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace Ejercicio.Web.Models
+{
+    public class EdicionRol
+    {
+        public IdentityRole Rol { get; set; }
+        public IEnumerable<IdentityUser> miembros { get; set; }
+        public IEnumerable<IdentityUser> noMiembros { get; set; }
+    }
+}
